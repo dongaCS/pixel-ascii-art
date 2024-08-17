b582fa15fd8f78a81605a6c6d09b5a0fab38382d
dongaCS <109496734+dongaCS@users.noreply.github.com>
2024-08-16 23:54:42 -0400

    working pixelation of gif

---
+++ gif-sketch.js
@@ -0,0 +1,31 @@
+let img;
+let pixelation_level = 8;
+
+function preload() {
+  img = loadImage("./images/fish.gif")
+}
+
+function setup() {
+  createCanvas(700, 350); // set width and height 
+  pixelDensity(1); // compress pixels
+  noStroke(); // hides grid
+}
+
+function draw() {
+  image(img, 0, 0, width, height); // draws the image
+  loadPixels(); // load the pixels array.
+
+  for (let x = 0; x < width; x += pixelation_level) {
+    for (let y = 0; y < height; y += pixelation_level) {
+      
+      let i = (x + y * width) * 4; // pixel index
+      let r = pixels[i + 0]; // loaded via loadPixels
+      let g = pixels[i + 1];
+      let b = pixels[i + 2];
+
+      fill(r, g, b) // pixels with colors
+      square(x, y, pixelation_level);
+    }
+  }
+
+}
